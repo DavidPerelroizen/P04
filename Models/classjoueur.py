33eaf3f9aa2fa7a4b8272460db6d2dac66c0571a
David Perelroizen <david.perelroizen@gmail.com>
2022-01-03 15:30:53 +0100

    Initiated the Joueur class

---
+++ Models/classjoueur.py
@@ -0,0 +1,9 @@
+class Joueur:
+
+    def __init__(self, last_name, first_name, birth_date, gender, rank):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.birth_date = birth_date
+        self.gender = gender
+        self.rank = rank
+
