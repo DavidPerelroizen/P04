27408b405b14fb79bcd17cc0de46173a894012c4
David Perelroizen <david.perelroizen@gmail.com>
2022-01-05 10:17:54 +0100

    Deleted the initialranking function as it's not needed

--- Models/classclassement.py
+++ Models/classclassement.py
@@ -6,11 +6,6 @@ class Classement:
     def __init__(self, players_ranking):
         self.players_ranking = players_ranking
 
-    def initialranking(self, players_list):
-        """This function sorts the players list according to the players ranks"""
-        self.players_ranking = sorted(players_list, key=operator.itemgetter(5))
-        return self.players_ranking
-
     def ranking(self, players_list):
         """This function sorts the players list according to their score and then according to their rank"""
         self.players_ranking = sorted(players_list, key=operator.itemgetter(6, 5))
