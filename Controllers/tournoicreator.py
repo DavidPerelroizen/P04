a581ca3136d9e0196cea7e7021be22b9a3800840
David Perelroizen <david.perelroizen@gmail.com>
2022-01-05 16:36:38 +0100

    Created a controller that generates the tournament

---
+++ Controllers/tournoicreator.py
@@ -0,0 +1,22 @@
+from Models.classtournoi import Tournoi
+from Models.constants import time_controllers
+
+
+def tournoicreation():
+    """This function initializes the creation of a tournament"""
+    tournoi_name = input('Enter the tournament name : ')
+    tournoi_place = input('Enter the tournament location : ')
+    tournoi_date = input('Enter the tournament date (JJ/MM/AAAA) : ')
+    tournoi_description = input('Enter the tournament description : ')
+    tournoi_time_controller = ''
+    while tournoi_time_controller not in time_controllers:
+        tournoi_time_controller = input('Enter a time controller (blitz, coup rapide, bullet) : ')
+
+    tournoi = Tournoi(tournoi_name, tournoi_place, tournoi_date, [], tournoi_description, tournoi_time_controller)
+
+    return tournoi
+
+
+
+
+
