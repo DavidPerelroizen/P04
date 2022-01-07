e51c8be44019e6d20a5c42ae08003375ccb05df2
David Perelroizen <david.perelroizen@gmail.com>
2022-01-07 19:01:59 +0100

    Initiated the main programm

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from Views.classviews import View
+from Controllers.tournoicreator import tournoicreation
+from Controllers.playersmanager import gettheplayers
+from Controllers.generatepairs import generatepairs
+from Models.classclassement import Classement
+from Controllers.roundmanager import roundmanager
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    view = View()
+    user_choice = view.displaymainmenu()
+
+    if user_choice == 'T':
+        """Tournament management"""
+        #  Tournament creation
+        tournoi = tournoicreation()
+        #  Players creation
+        players_number = int(input('How many players do you want to add?: '))
+        tournoi.players_list = gettheplayers(players_number)
+        print("""
+        
+        """)
+        #  Launch the rounds
+        rounds_number = int(input('How many rounds do you want to play?: '))
+        print("""
+
+        """)
+        for i in range(rounds_number):
+            players_ranking = Classement()
+            players_ranking.ranking(tournoi.players_list)
+            pairs_list = generatepairs(players_ranking, tournoi.rounds_list, i)
+            round_match_list = roundmanager(i, pairs_list)
+            tournoi.rounds_list.append(round_match_list)
+            view.displayroundresult(i)
+            print("""
+
+            """)
+
+
+    else:
+        """Reporting management"""
+        view.displayreportingmenu()
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
