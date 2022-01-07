28546330d99aedbe830cd57588acae936d8214a2
David Perelroizen <david.perelroizen@gmail.com>
2022-01-07 15:28:37 +0100

    Initiated the reporting module

---
+++ Views/classreport.py
@@ -0,0 +1,14 @@
+
+class Report:
+
+    def allplayerslist(self):
+        pass
+
+    def tournamentplayerslist(self, tournament_name):
+        pass
+
+    def tournamentroundslist(self, tournament_name):
+        pass
+
+    def tournamentmatcheslist(self, tournament_name):
+        pass
