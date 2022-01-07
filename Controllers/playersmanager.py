0964075545cf95ac12bfa96fd793b12b11c8ba34
David Perelroizen <david.perelroizen@gmail.com>
2022-01-07 19:03:01 +0100

    Modified the player type from a Joueur object to a list to avoid Type errors

--- Controllers/playersmanager.py
+++ Controllers/playersmanager.py
@@ -6,6 +6,9 @@ def gettheplayers(players_number):
     players_list = []
 
     for i in range(1, players_number+1):
+        print("""
+
+        """)
         """player_index, last_name, first_name, birth_date, gender, rank"""
         player_index = "Player" + str(i)
         print('Prepare to fill in information for ', player_index)
@@ -21,10 +24,16 @@ def gettheplayers(players_number):
                 player_rank = int(input(f'Enter the player rank (only integers from 1 to {players_number}) : '))
             except ValueError:
                 print('Integers only from 1 to ', players_number)
+        player_score = 0
 
-        player = Joueur(
-            player_index, player_last_name, player_first_name, player_birth_date, player_gender, player_rank
-        )
+        print("""
+
+        """)
+
+        player = [
+            player_index, player_last_name, player_first_name, player_birth_date, player_gender, player_rank,
+            player_score
+        ]
 
         players_list.append(player)
 
