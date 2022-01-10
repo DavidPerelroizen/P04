60c6d7c98f329870b877d06e9bdc9b0a9b02465d
David Perelroizen <david.perelroizen@gmail.com>
2022-01-10 16:54:52 +0100

    Designed a function that will display all the players list either alphabetically or by rank

--- Views/classviews.py
+++ Views/classviews.py
@@ -1,4 +1,6 @@
 from Models.classtour import Tour
+from Models.classtournoi import Tournoi
+
 
 class View:
 
@@ -10,7 +12,7 @@ class View:
             """
             CHESS APPLICATOR
         ----------------------------------
-                MAIN MENU
+                MAIN MENUE
         ----------------------------------
         1. Tournament manager --> press T
         2. Reports --> press R
@@ -29,13 +31,14 @@ class View:
             """
                     CHESS APPLICATOR
         --------------------------------------------
-                     REPORTING MENU
+                     REPORTING MENUE
         --------------------------------------------
         1. All players list --> press A
         2. Players of a tournament list --> press P
         3. All tournaments list --> press T
         4. Tournament all rounds list --> press R
-        5. Tournament all matches list --> press M        
+        5. Tournament all matches list --> press M 
+        6. Back to main menue --> press B   
         --------------------------------------------""")
         user_choice = input('        Press the appropriate key + ENTER : ').upper()
 
@@ -48,8 +51,29 @@ class View:
         user_choice = input('Do you want to update the players ranking? (Yes/No)').upper()
         return user_choice
 
-    def displayallplayerslist(self):
-        pass
+    def displayallplayerslist(self, tournaments_list):
+        user_choice = input('Alphabetical or ranking display?: ').upper()
+        if user_choice == 'ALPHABETICAL':
+            print(f"Display all players list in alphabetical order")
+            reporting_list = []
+            for tournament in tournaments_list:
+                for player in tournament.players_list:
+                    reporting_list.append(player)
+            reporting_list.sort(key=lambda x: x[1])
+            for item in reporting_list:
+                print(item)
 
-    def displaytournamentplayers(self, tournament_name):
-        pass
+        else:
+            print(f"Display all players list in ranking order")
+            reporting_list = []
+            for tournament in tournaments_list:
+                for player in tournament.players_list:
+                    reporting_list.append(player)
+            reporting_list_sorted = sorted(reporting_list, key=lambda x: x[5])
+            for item in reporting_list_sorted:
+                print(item)
+
+    def displaytournamentplayers(self, tournament):
+        print(f"Display {tournament.name} players list")
+        for player in tournament.players_list:
+            print(player)
