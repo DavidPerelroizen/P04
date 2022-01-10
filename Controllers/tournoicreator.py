e4618332a62f4009efbe0c347a7a2ac25762d8fd
David Perelroizen <david.perelroizen@gmail.com>
2022-01-10 15:31:40 +0100

    Added input controls on the date

--- Controllers/tournoicreator.py
+++ Controllers/tournoicreator.py
@@ -6,7 +6,12 @@ def tournoicreation():
     """This function initializes the creation of a tournament"""
     tournoi_name = input('Enter the tournament name : ')
     tournoi_place = input('Enter the tournament location : ')
-    tournoi_date = input('Enter the tournament date (JJ/MM/AAAA) : ')
+    tournoi_date = ''
+    while len(tournoi_date) != 10:
+        try:
+            tournoi_date = input('Enter the tournament date (JJ/MM/AAAA) : ')
+        except ValueError:
+            print("Please enter a valid date")
     tournoi_description = input('Enter the tournament description : ')
     tournoi_time_controller = ''
     while tournoi_time_controller not in time_controllers:
