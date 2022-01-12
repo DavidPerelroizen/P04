461ed8edc5b44f7dd8e863b473a0aee0a4bbb6dc
David Perelroizen <david.perelroizen@gmail.com>
2022-01-12 15:27:11 +0100

    Modified the tournoicreation function in order to secure the input of a valid key for the time controller choice

--- Controllers/tournoicreator.py
+++ Controllers/tournoicreator.py
@@ -15,9 +15,14 @@ def tournoicreation():
     tournoi_description = input('Enter the tournament description : ')
     tournoi_time_controller = ''
     while tournoi_time_controller == '':
-        tournoi_time_controller = time_controllers_dict[int(input(
+        try:
+            tournoi_time_controller = time_controllers_dict[int(input(
             'Enter a time controller (blitz--> 1, coup rapide --> 2, bullet --> 3) : '))]
-        print(f'Time controller selected: {tournoi_time_controller}')
+        except KeyError:
+            print('Please enter a valid key')
+        except ValueError:
+            print('Please enter a valid key')
+    print(f'Time controller selected: {tournoi_time_controller}')
 
     tournoi = Tournoi(tournoi_name, tournoi_place, tournoi_date, [], tournoi_description, tournoi_time_controller)
 
