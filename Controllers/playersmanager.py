14642d7a286b01d64c10ca5e426a11e75d55af2e
David Perelroizen <david.perelroizen@gmail.com>
2022-01-12 20:09:27 +0100

    Secured the input of the player birthday information

--- Controllers/playersmanager.py
+++ Controllers/playersmanager.py
@@ -1,5 +1,6 @@
 from Models.classjoueur import Joueur
 from Models.constants import gender_dict
+import datetime
 
 
 def gettheplayers(players_number):
@@ -14,12 +15,26 @@ def gettheplayers(players_number):
         print('Prepare to fill in information for ', player_index)
         player_last_name = input('Enter the player last name : ').upper()
         player_first_name = input('Enter the player first name : ').lower()
-        player_birth_date = ''
-        while len(player_birth_date) != 10:
+        print(f'Prepare to fill information {player_index} birth date: ')
+        player_birth_day = ''
+        while player_birth_day not in range(1, 32):
             try:
-                player_birth_date = input('Enter the player birth date (JJ/MM/AAAA) : ')
+                player_birth_day = int(input('Enter the day: '))
             except ValueError:
-                print("Please enter a valid birth date")
+                print('Please enter a valid day (integer from 1 to 31)')
+        player_birth_month = ''
+        while player_birth_month not in range(1, 13):
+            try:
+                player_birth_month = int(input('Enter the month (integer from 1 to 12): '))
+            except ValueError:
+                print('Please enter a valid month (integer from 1 to 12)')
+        player_birth_year = ''
+        while player_birth_year not in range(1900, 9999):
+            try:
+                player_birth_year = int(input('Enter the year (four digits integer): '))
+            except ValueError:
+                print('Please enter a valid year')
+        player_birth_date = datetime.date(player_birth_year, player_birth_month, player_birth_day)
         player_gender = ''
         while player_gender == '':
             try:
