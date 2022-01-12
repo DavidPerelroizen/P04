624f7ab43cf7edc75b674a0c8a692ea9231c2715
David Perelroizen <david.perelroizen@gmail.com>
2022-01-12 15:11:08 +0100

    Modified the generatepairs function so that it would work for any even number of players

--- Controllers/generatepairs.py
+++ Controllers/generatepairs.py
@@ -5,12 +5,8 @@ def generatepairs(players_ranking, rounds_list, round_number, number_of_players=
     """
     new_pairs_list = []
     if round_number == 1:
-        new_pairs_list = [
-            (players_ranking[0], players_ranking[4]),
-            (players_ranking[1], players_ranking[5]),
-            (players_ranking[2], players_ranking[6]),
-            (players_ranking[3], players_ranking[7])
-        ]
+        for i in range(int(number_of_players/2)):
+            new_pairs_list.append((players_ranking[i], players_ranking[int((number_of_players/2) + i)]))
 
     else:
         i = 0
