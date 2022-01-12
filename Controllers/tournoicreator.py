dcb8f34d6bcc22223156d9dbc3daaead8c83a636
David Perelroizen <david.perelroizen@gmail.com>
2022-01-12 20:48:55 +0100

    Added checks and controls to make sure that user input for date has the correct format and is consistent

--- Controllers/tournoicreator.py
+++ Controllers/tournoicreator.py
@@ -8,31 +8,36 @@ def tournoicreation():
 
     tournoi_name = input('Enter the tournament name : ')
     tournoi_place = input('Enter the tournament location : ')
-    tournoi_day = ''
-    while tournoi_day not in range(1, 32):
-        try:
-            tournoi_day = int(input('Enter the day: '))
-        except ValueError:
-            print('Please enter a valid day (integer from 1 to 31)')
-    tournoi_month = ''
-    while tournoi_month not in range(1, 13):
-        try:
-            tournoi_month = int(input('Enter the month (integer from 1 to 12): '))
-        except ValueError:
-            print('Please enter a valid month (integer from 1 to 12)')
-    tournoi_year = ''
-    while tournoi_year not in range(1900, 9999):
-        try:
-            tournoi_year = int(input('Enter the year (four digits integer): '))
-        except ValueError:
-            print('Please enter a valid year')
-    tournoi_date = datetime.date(tournoi_year, tournoi_month, tournoi_day)
+
+    #  Define the date with checks and controls
+    tournoi_date = datetime.date(1900, 1, 1)
+    while tournoi_date < datetime.date.today():
+        tournoi_day = ''
+        while tournoi_day not in range(1, 32):
+            try:
+                tournoi_day = int(input('Enter the day: '))
+            except ValueError:
+                print('Please enter a valid day (integer from 1 to 31)')
+        tournoi_month = ''
+        while tournoi_month not in range(1, 13):
+            try:
+                tournoi_month = int(input('Enter the month (integer from 1 to 12): '))
+            except ValueError:
+                print('Please enter a valid month (integer from 1 to 12)')
+        tournoi_year = ''
+        while tournoi_year not in range(1900, 9999):
+            try:
+                tournoi_year = int(input('Enter the year (four digits integer): '))
+            except ValueError:
+                print('Please enter a valid year')
+        tournoi_date = datetime.date(tournoi_year, tournoi_month, tournoi_day)
+
     tournoi_description = input('Enter the tournament description : ')
     tournoi_time_controller = ''
     while tournoi_time_controller == '':
         try:
-            tournoi_time_controller = time_controllers_dict[int(input(
-            'Enter a time controller (blitz--> 1, coup rapide --> 2, bullet --> 3) : '))]
+            tournoi_time_controller = time_controllers_dict[int(
+                input('Enter a time controller (blitz--> 1, coup rapide --> 2, bullet --> 3) : '))]
         except KeyError:
             print('Please enter a valid key')
         except ValueError:
