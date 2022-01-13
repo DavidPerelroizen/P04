721c7ad0194a46951e43b41739b56c6bade2c58e
David Perelroizen <david.perelroizen@gmail.com>
2022-01-13 09:47:18 +0100

    Added the player index to the serialized data

--- Controllers/dbplayers.py
+++ Controllers/dbplayers.py
@@ -8,10 +8,12 @@ playersdb.truncate()
 
 
 def serializeplayer(player_for_serializing):
-    """This fucntion aims at serializing a player (class = Joueur()) and insert is in the tiny DB"""
+    """This function aims at serializing a player (class = Joueur()) and insert is in the tiny DB"""
     player_dict = {
-        'last_name': player_for_serializing.last_name, 'first_name': player_for_serializing.first_name,
-        'birth_date': json.dumps(player_for_serializing.birth_date, default=str), 'gender': player_for_serializing.gender,
+        'player_index': player_for_serializing.player_index, 'last_name': player_for_serializing.last_name,
+        'first_name': player_for_serializing.first_name,
+        'birth_date': json.dumps(player_for_serializing.birth_date, default=str),
+        'gender': player_for_serializing.gender,
         'rank': player_for_serializing.rank, 'score': player_for_serializing.score
     }
     playersdb.insert(player_dict)
