28a676d94866243e4d9aab51b3ee2b52adeedf49
David Perelroizen <david.perelroizen@gmail.com>
2022-01-17 20:15:22 +0100

    Cleaned the file and added comments

--- Controllers/dbplayers.py
+++ Controllers/dbplayers.py
@@ -5,6 +5,7 @@ from tinydb import TinyDB, Query
 db = TinyDB('db.json')
 players_table = db.table('players')
 
+
 def serializeplayer(player_for_serializing):
     """This function aims at serializing a player (class = Joueur()) and insert is in the tiny DB"""
     player_dict = {
@@ -17,6 +18,7 @@ def serializeplayer(player_for_serializing):
 
     players_table.insert(player_dict)
 
+
 def deserializeallplayers():
     """
     This function aims at deserializing the all the players data from the players_table in order to use it
@@ -26,8 +28,8 @@ def deserializeallplayers():
     deserialized_players = players_table.all()
 
     for dico in deserialized_players:
-        birth_date  = dico['birth_date']
-        birth_date = birth_date[1: -2]
+        birth_date = dico['birth_date']
+        birth_date = birth_date[1: -2]  # Avoid over quoting of string
         player_info_list = [
                             dico['player_index'], dico['last_name'], dico['first_name'], birth_date,
                             dico['gender'], dico['rank'], dico['score']
@@ -36,13 +38,14 @@ def deserializeallplayers():
 
     return deserialized_players_list
 
+
 def deserializeplayersdicos(list_of_dicos):
     """This function deserializes a list of dictionaries containing players instances"""
     deserialized_players_list = []
 
     for dico in list_of_dicos:
-        birth_date  = dico['birth_date']
-        birth_date = birth_date[1: -2]
+        birth_date = dico['birth_date']
+        birth_date = birth_date[1: -2]  # Avoid over quoting of string
         player_info_list = [
             dico['player_index'], dico['last_name'], dico['first_name'], birth_date,
             dico['gender'], dico['rank'], dico['score']
@@ -54,10 +57,11 @@ def deserializeplayersdicos(list_of_dicos):
 
 def updateplayersscore(player_index_researched, score_update):
     """Function that updates a given player's score depending on his index"""
-    Playerquery = Query()
-    players_table.update({'score': score_update}, Playerquery.player_index == player_index_researched)
+    playerquery = Query()
+    players_table.update({'score': score_update}, playerquery.player_index == player_index_researched)
+
 
 def updateplayersrank(player_index_researched, rank_update):
     """Function that updates a given player's rank depending on his index"""
-    Playerquery = Query()
-    players_table.update({'rank': rank_update}, Playerquery.player_index == player_index_researched)
+    playerquery = Query()
+    players_table.update({'rank': rank_update}, playerquery.player_index == player_index_researched)
