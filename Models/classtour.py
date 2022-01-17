68b0e3b0dc1994cff967106ce001728399678e8a
David Perelroizen <david.perelroizen@gmail.com>
2022-01-17 19:50:23 +0100

    Added comments, cleaned the file

--- Models/classtour.py
+++ Models/classtour.py
@@ -2,6 +2,7 @@ import datetime
 
 
 class Tour:
+    """This class aims at modeling a round inside a tournament"""
 
     def __init__(self, round_name, date_time_begin=0, date_time_finish=0, matches_list=[]):
         self.round_name = round_name
@@ -10,14 +11,16 @@ class Tour:
         self.matches_list = matches_list
 
     def initiateround(self):
+        """This function will automatically return the starting time of the round"""
         self.date_time_begin = datetime.datetime.today().replace(microsecond=0)
         return self.date_time_begin
 
     def addmatch(self, match_tuple):
+        """This function will a match instance to the list of matches of the round"""
         self.matches_list.append(match_tuple)
         return self.matches_list
 
     def finishround(self):
+        """This function will automatically return the starting time of the round"""
         self.date_time_finish = datetime.datetime.today().replace(microsecond=0)
         return self.date_time_finish
-
