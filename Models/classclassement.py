e37afc22826541517fefa723dd33769678e64fb5
David Perelroizen <david.perelroizen@gmail.com>
2022-01-17 19:55:39 +0100

    Added comments, cleaned the file

--- Models/classclassement.py
+++ Models/classclassement.py
@@ -1,13 +1,8 @@
 
 class Classement:
-    """
-    def __init__(self, players_ranking=[]):
-        self.players_ranking = players_ranking
-    """
+    """This object will help refreshing the players ranking"""
+
     def ranking(self, players_list):
         """This function sorts the players list according to their score and then according to their rank"""
         players_ranking = sorted(players_list, key=lambda x:  (-x[6], x[5]))
         return players_ranking
-
-
-
